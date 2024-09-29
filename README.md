f87fa784274589d95c9cd3c4acef1c88771e0e3f
happyincent <happyincent@gmail.com>
2024-09-30 00:38:07 +0800

    bumps & replace exifr to exifreader

--- README.md
+++ README.md
@@ -3,20 +3,17 @@
 ## Introduction
 Upload photos with caption e.g., `#20200913 #Taipei (Pixel 3a XL)` to Instagram in one click.
 
-* Take image metadata as Feed Caption ([exifr](https://github.com/MikeKovarik/exifr))
+* Take image metadata as Feed Caption ([exifreader](https://github.com/mattiasw/ExifReader))
 * Reverse Latitude and Longitude into City name with OpenStreetMap ([Nominatim API](http://nominatim.org/release-docs/latest/api/Reverse/))
 * Crop image if `width < height` in a [4:5](https://help.instagram.com/1631821640426723) aspect ratio ([sharp](https://github.com/lovell/sharp))
 * Login and upload photos to Instagram Feed ([instagram-private-api](https://github.com/dilame/instagram-private-api))
 
 ## Prerequisite
-* [Node.js](https://nodejs.org/en/)
-* [TypeScript](https://www.typescriptlang.org/#installation)
-
-## Usage
-#### 0. Installation
 ```
 npm install
 ```
+
+## Usage
 
 #### 1. Add .env into `./.env`
 ```
--- index.ts
+++ index.ts
@@ -1,10 +1,11 @@
 import dayjs from "dayjs";
 import dotenv from "dotenv";
-import exifr from "exifr";
 import fs from "fs";
 import path from "path";
 import { createIgClient, createIgPhoto } from "./ig-client";
 import { reverse } from "./nominatim";
+import ExifReader from "exifreader";
+import customParseFormat from "dayjs/plugin/customParseFormat";
 
 const DEBUG = false;
 const DELAY = 1000 * 1;
@@ -12,6 +13,7 @@ const FOLDER = path.resolve("./img/");
 const FOLDER_ED = path.join(FOLDER, "ed");
 fs.existsSync(FOLDER_ED) || fs.mkdirSync(FOLDER_ED, { recursive: true });
 dotenv.config();
+dayjs.extend(customParseFormat);
 
 (async () => {
   try {
@@ -22,37 +24,40 @@ dotenv.config();
       .filter((file) => file.isFile())
       .map((file) => path.join(file.parentPath, file.name));
 
-    let filesInfo = (
+    const filesInfo = (
       await Promise.all(
-        files.map(async (file) => {
-          const res = await exifr.parse(file, ["DateTimeOriginal", "Model", "GPSLatitude", "GPSLongitude"]);
-          return { ...res, file: file } as {
-            file: string;
-            DateTimeOriginal?: Date;
-            Model?: string;
-            latitude?: number;
-            longitude?: number;
-          };
-        })
+        files.map((file) =>
+          ExifReader.load(file, { expanded: true }).then((tags) => ({
+            file,
+            DateTime: tags.exif?.DateTimeOriginal?.description
+              ? dayjs(tags.exif.DateTimeOriginal.description, "YYYY:MM:DD HH:mm:ss").toDate()
+              : fs.statSync(file).mtime,
+            Latitude: tags.gps?.Latitude,
+            Longitude: tags.gps?.Longitude,
+            Model: tags.exif?.Model?.description.replaceAll(" ", "").replaceAll("-", ""),
+          }))
+        )
       )
-    ).sort((a, b) => dayjs(a.DateTimeOriginal).diff(dayjs(b.DateTimeOriginal)));
+    ).sort((a, b) => dayjs(a.DateTime).diff(dayjs(b.DateTime)));
 
     for (const [index, info] of Object.entries(filesInfo)) {
       await new Promise((r) => setTimeout(r, DELAY));
 
       let caption: string = "";
-      if (info.DateTimeOriginal) caption += `#${dayjs(info.DateTimeOriginal).format("YYYYMMDD")}`;
-      if (info.latitude && info.longitude) {
-        const res = await reverse({ lat: info.latitude, lon: info.longitude, language: "en" });
+      if (info.DateTime) caption += `#${dayjs(info.DateTime).format("YYYYMMDD")}`;
+      if (info.Latitude && info.Longitude) {
+        const res = await reverse({ lat: info.Latitude, lon: info.Longitude, language: "en" });
         const { town, suburb, city, state_district, county, state, country } = res.address;
         const place = [town, suburb, city, state_district, county, state, country]
           .find((val) => Boolean(val))
-          ?.replace("New Taipei", "NewTaipei")
-          ?.replace("'", "")
+          ?.replace("New ", "New")
+          ?.replaceAll("'", "")
           ?.split(" ")[0];
-        if (place) caption += ` #${place}`;
+        if (place)
+          if (["North", "South", "East", "West"].includes(place) && city) caption += ` #${place}${city}`;
+          else caption += ` #${place}`;
       }
-      if (info.Model) caption += ` (${info.Model})`;
+      if (info.Model) caption += ` #${info.Model}`;
 
       await igClient.publishPhoto({ file: await createIgPhoto(info.file), caption: caption });
       fs.renameSync(info.file, path.join(FOLDER_ED, path.basename(info.file)));
